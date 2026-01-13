d6d64cec67fdffb32fc3ec98c40faf5c0c144a5e
Tibor Sohonyai <185492254+Rocka06@users.noreply.github.com>
2026-01-13 08:55:14 +0100

    added tests for InviteController

---
+++ src/test/java/com/shelflife/project/invitecontroller/GetInvitesTests.java
@@ -0,0 +1,121 @@
+package com.shelflife.project.invitecontroller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.shelflife.project.model.Storage;
+import com.shelflife.project.model.StorageMember;
+import com.shelflife.project.model.User;
+import com.shelflife.project.repository.StorageMemberRepository;
+import com.shelflife.project.repository.StorageRepository;
+import com.shelflife.project.repository.UserRepository;
+import com.shelflife.project.service.JwtService;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.transaction.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import static org.hamcrest.Matchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@Transactional
+public class GetInvitesTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private StorageRepository storageRepository;
+
+    @Autowired
+    private StorageMemberRepository storageMemberRepository;
+
+    private User testAdmin;
+    private User testUser;
+    private User testMember;
+
+    private Storage testUserStorage;
+
+    private StorageMember testInvite;
+
+    @BeforeEach
+    void setup() {
+        testAdmin = new User();
+        testAdmin.setEmail("test@test.test");
+        testAdmin.setUsername("test");
+        testAdmin.setPassword("test123");
+        testAdmin.setAdmin(true);
+        testAdmin = userRepository.save(testAdmin);
+
+        testUser = new User();
+        testUser.setEmail("testuser@test.test");
+        testUser.setUsername("testuser");
+        testUser.setPassword("test123");
+        testUser.setAdmin(false);
+        testUser = userRepository.save(testUser);
+
+        testMember = new User();
+        testMember.setEmail("testmember@test.test");
+        testMember.setUsername("testmember");
+        testMember.setPassword("test123");
+        testMember.setAdmin(false);
+        testMember = userRepository.save(testMember);
+
+        testUserStorage = new Storage();
+        testUserStorage.setOwner(testUser);
+        testUserStorage.setName("userTest");
+        testUserStorage = storageRepository.save(testUserStorage);
+
+        testInvite = new StorageMember();
+        testInvite.setStorage(testUserStorage);
+        testInvite.setUser(testMember);
+        testInvite.setAccepted(false);
+        testInvite = storageMemberRepository.save(testInvite);
+    }
+
+    @Test
+    void returnsOneAsAuthenticated() throws Exception {
+        String jwt = jwtService.generateToken(testMember.getEmail());
+        Cookie jwtCookie = new Cookie("jwt", jwt);
+
+        mockMvc.perform(get("/api/storages/invites")
+                .cookie(jwtCookie))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].id").value(testInvite.getId()))
+                .andExpect(jsonPath("$[0].storage.id").value(testInvite.getStorage().getId()))
+                .andExpect(jsonPath("$[0].user.id").value(testInvite.getUser().getId()))
+                .andExpect(jsonPath("$[0].accepted").value(false));
+    }
+
+    @Test
+    void returnsZeroAsAuthenticated() throws Exception {
+        String jwt = jwtService.generateToken(testAdmin.getEmail());
+        Cookie jwtCookie = new Cookie("jwt", jwt);
+
+        mockMvc.perform(get("/api/storages/invites")
+                .cookie(jwtCookie))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(0)));
+    }
+
+    @Test
+    void accessDeniedAsAnonymous() throws Exception {
+        mockMvc.perform(get("/api/storages/invites"))
+                .andExpect(status().isForbidden());
+    }
+}

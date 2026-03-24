01dd35f2a5bde075e945b6fc5cf31bc9b1346dab
Tibor Sohonyai <185492254+Rocka06@users.noreply.github.com>
2026-03-24 21:49:43 +0100

    fixed migration errors

--- src/main/resources/db/migration/V4__add_ondelete_cascade.sql
+++ src/main/resources/db/migration/V4__add_ondelete_cascade.sql
@@ -1,74 +1,103 @@
-ALTER TABLE storage_member
-DROP CONSTRAINT IF EXISTS fk_storage_member_user,
-ADD CONSTRAINT fk_storage_member_user
+-- Member
+ALTER TABLE storage_members
+DROP FOREIGN KEY fk_storage_members_user;
+
+ALTER TABLE storage_members
+ADD CONSTRAINT fk_storage_members_user
 FOREIGN KEY (user_id)
 REFERENCES users(id)
 ON DELETE CASCADE;
 
-ALTER TABLE storage_member
-DROP CONSTRAINT IF EXISTS fk_storage_member_storage,
-ADD CONSTRAINT fk_storage_member_storage
+
+ALTER TABLE storage_members
+DROP FOREIGN KEY fk_storage_members_storage;
+
+ALTER TABLE storage_members
+ADD CONSTRAINT fk_storage_members_storage
 FOREIGN KEY (storage_id)
 REFERENCES storages(id)
 ON DELETE CASCADE;
 
+
+-- Storages
+ALTER TABLE storages
+DROP FOREIGN KEY fk_storages_owner;
 
 ALTER TABLE storages
-DROP CONSTRAINT IF EXISTS fk_storages_user,
-ADD CONSTRAINT fk_storages_user
+ADD CONSTRAINT fk_storages_owner
 FOREIGN KEY (owner_id)
 REFERENCES users(id)
 ON DELETE CASCADE;
 
+
+-- Products
+ALTER TABLE products
+DROP FOREIGN KEY fk_products_owner;
 
 ALTER TABLE products
-DROP CONSTRAINT IF EXISTS fk_products_user,
-ADD CONSTRAINT fk_products_user
+ADD CONSTRAINT fk_products_owner
 FOREIGN KEY (owner_id)
 REFERENCES users(id)
 ON DELETE CASCADE;
 
 
-ALTER TABLE storage_item
-DROP CONSTRAINT IF EXISTS fk_storage_item_storage,
-ADD CONSTRAINT fk_storage_item_storage
+-- Storage Item
+ALTER TABLE storage_items
+DROP FOREIGN KEY fk_storage_items_storage;
+
+ALTER TABLE storage_items
+ADD CONSTRAINT fk_storage_items_storage
 FOREIGN KEY (storage_id)
 REFERENCES storages(id)
 ON DELETE CASCADE;
 
-ALTER TABLE storage_item
-DROP CONSTRAINT IF EXISTS fk_storage_item_product,
-ADD CONSTRAINT fk_storage_item_product
+
+ALTER TABLE storage_items
+DROP FOREIGN KEY fk_storage_items_product;
+
+ALTER TABLE storage_items
+ADD CONSTRAINT fk_storage_items_product
+FOREIGN KEY (product_id)
+REFERENCES products(id)
+ON DELETE CASCADE;
+
+-- Running Low
+ALTER TABLE running_low_settings
+DROP FOREIGN KEY fk_running_low_storage;
+
+ALTER TABLE running_low_settings
+ADD CONSTRAINT fk_running_low_storage
+FOREIGN KEY (storage_id)
+REFERENCES storages(id)
+ON DELETE CASCADE;
+
+
+ALTER TABLE running_low_settings
+DROP FOREIGN KEY fk_running_low_product;
+
+ALTER TABLE running_low_settings
+ADD CONSTRAINT fk_running_low_product
 FOREIGN KEY (product_id)
 REFERENCES products(id)
 ON DELETE CASCADE;
 
 
-ALTER TABLE running_low_setting
-DROP CONSTRAINT IF EXISTS fk_running_low_setting_storage,
-ADD CONSTRAINT fk_running_low_setting_storage
+-- Shopping Item
+ALTER TABLE shopping_list_items
+DROP FOREIGN KEY fk_shopping_storage;
+
+ALTER TABLE shopping_list_items
+ADD CONSTRAINT fk_shopping_storage
 FOREIGN KEY (storage_id)
 REFERENCES storages(id)
 ON DELETE CASCADE;
 
-ALTER TABLE running_low_setting
-DROP CONSTRAINT IF EXISTS fk_running_low_setting_product,
-ADD CONSTRAINT fk_running_low_setting_product
-FOREIGN KEY (product_id)
-REFERENCES products(id)
-ON DELETE CASCADE;
 
+ALTER TABLE shopping_list_items
+DROP FOREIGN KEY fk_shopping_product;
 
-ALTER TABLE shopping_list_item
-DROP CONSTRAINT IF EXISTS fk_shopping_list_item_storage,
-ADD CONSTRAINT fk_shopping_list_item_storage
-FOREIGN KEY (storage_id)
-REFERENCES storages(id)
-ON DELETE CASCADE;
-
-ALTER TABLE shopping_list_item
-DROP CONSTRAINT IF EXISTS fk_shopping_list_item_product,
-ADD CONSTRAINT fk_shopping_list_item_product
+ALTER TABLE shopping_list_items
+ADD CONSTRAINT fk_shopping_product
 FOREIGN KEY (product_id)
 REFERENCES products(id)
 ON DELETE CASCADE;
